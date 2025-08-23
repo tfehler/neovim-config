06d7756a32c4d752f1846f39abd72018091fb2a6
Tim Fehler <126073245+TimFehler@users.noreply.github.com>
2025-08-23 08:33:58 +0200

    Fixing view method for vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,7 +2,7 @@ return {
     "lervag/vimtex",
     lazy = false, -- lazy-loading will disable inverse search
     config = function()
-        vim.g.vimtex_view_method = "zathuara"
+        vim.g.vimtex_view_method = "zathura"
     end,
     keys = {
         { "<localLeader>l", "", desc = "+vimtex" },
